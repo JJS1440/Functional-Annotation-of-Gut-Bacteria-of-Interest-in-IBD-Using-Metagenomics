7223a1de64449f03596c48b1fd029a31e53287fe
Jaume <116804126+JJS1440@users.noreply.github.com>
2026-06-20 00:46:18 +0200

    Update Kraken_db_building.sh

--- Scripts/3-Kraken2/Kraken_db_building.sh
+++ Scripts/3-Kraken2/Kraken_db_building.sh
@@ -19,7 +19,7 @@ ulimit -s unlimited
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 
-OUTPUT_DIR="/home/BITamonleon/JaumeJurado/Chron/Data/Outputs/Kraken_db/"
+OUTPUT_DIR="/home/BITamonleon/JaumeJurado/TFG/Data/Outputs/Kraken_db/"
 
 # Checking if the directory exists, if not the script crates it
 if [ -d "$OUTPUT_DIR" ]; then
@@ -31,11 +31,11 @@ else
     echo "The directory $OUTPUT_DIR has been created."
 fi
 
-#kraken2-build --download-taxonomy --db $OUTPUT_DIR
+kraken2-build --download-taxonomy --db $OUTPUT_DIR
 
-#kraken2-build --download-library human --db $OUTPUT_DIR
+kraken2-build --download-library human --db $OUTPUT_DIR
 
-#kraken2-build --download-library bacteria --db $OUTPUT_DIR
+kraken2-build --download-library bacteria --db $OUTPUT_DIR
 
 kraken2-build --build --db $OUTPUT_DIR --threads $THREADS
 
