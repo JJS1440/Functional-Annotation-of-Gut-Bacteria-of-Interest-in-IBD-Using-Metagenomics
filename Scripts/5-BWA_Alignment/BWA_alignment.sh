209e30939488bcf4b3cb8910cadfc3e94e08fd36
Jaume <116804126+JJS1440@users.noreply.github.com>
2026-06-20 00:47:40 +0200

    Add files via upload

---
+++ Scripts/5-BWA_Alignment/BWA_alignment.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+#SBATCH --job-name=BWA
+#SBATCH --output=/home/BITamonleon/JaumeJurado/Chron/Data/Outputs/BWA/log/Alignment/BWA_Index_%j.out
+#SBATCH --error=/home/BITamonleon/JaumeJurado/Chron/Data/Outputs/BWA/log/Alignment/BWA_Index_%j.err
+#SBATCH --cpus-per-task=1
+#SBATCH --partition=irbio01
+#SBATCH --array=0-20
+
+OUTPUT_PATH="/home/BITamonleon/JaumeJurado/Chron/Data/Outputs/BWA/Alignment"
+INDEX_FILES="/home/BITamonleon/JaumeJurado/Chron/Data/Outputs/BWA/Index"
+REF_GENOME_PATH="/home/BITamonleon/JaumeJurado/Chron/Data/Outputs/Genome_retriver/Ref_genomes"
+
+FILES_2_ALIGN="/home/BITamonleon/JaumeJurado/Chron/Data/Outputs/Filterer/Step4"
+
+. /etc/profile
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+module load anaconda/2024.02
+source activate jaume_env
+
+ulimit -s unlimited
+
+FILES=($(find "$FILES_2_ALIGN" -name "*.fastq"))
+
+FILES_NAMES=($(find "$FILES_2_ALIGN" -name "*.fastq" -printf "%f\n"))
+
+
+FIRST_PARTS=()
+
+for f in "${FILES_NAMES[@]}"; do
+    IFS='_.'
+    read -ra PARTS <<< "$f"
+    IFS=' '
+
+    ids+=("${PARTS[0]}")
+done
+
+unique_ids=($(printf "%s\n" "${ids[@]}" | sort -u))
+
+reference="$INDEX_FILES/${unique_ids[$SLURM_ARRAY_TASK_ID]}.fasta"
+echo "$reference"
+
+# Paired healthy
+paired1="$FILES_2_ALIGN/${unique_ids[$SLURM_ARRAY_TASK_ID]}_healthy_R1_paired.fastq"
+paired2="$FILES_2_ALIGN/${unique_ids[$SLURM_ARRAY_TASK_ID]}_healthy_R2_paired.fastq"
+output="$OUTPUT_PATH/${unique_ids[$SLURM_ARRAY_TASK_ID]}_healthy_paired_alignment.sam"
+
+bwa mem "$reference" "$paired1" "$paired2" > "$output"
+
+# Paired ill
+paired1="$FILES_2_ALIGN/${unique_ids[$SLURM_ARRAY_TASK_ID]}_ill_R1_paired.fastq"
+paired2="$FILES_2_ALIGN/${unique_ids[$SLURM_ARRAY_TASK_ID]}_ill_R2_paired.fastq"
+output="$OUTPUT_PATH/${unique_ids[$SLURM_ARRAY_TASK_ID]}_ill_paired_alignment.sam"
+
+bwa mem "$reference" "$paired1" "$paired2" > "$output"
+
+# Unpaired healthy
+unpaired="$FILES_2_ALIGN/${unique_ids[$SLURM_ARRAY_TASK_ID]}_healthy_unpaired.fastq"
+output="$OUTPUT_PATH/${unique_ids[$SLURM_ARRAY_TASK_ID]}_healthy_unpaired_alignment.sam"
+
+bwa mem "$reference" "$unpaired" > "$output"
+
+# Unpaired ill
+unpaired="$FILES_2_ALIGN/${unique_ids[$SLURM_ARRAY_TASK_ID]}_ill_unpaired.fastq"
+output="$OUTPUT_PATH/${unique_ids[$SLURM_ARRAY_TASK_ID]}_ill_unpaired_alignment.sam"
+
+bwa mem "$reference" "$unpaired" > "$output"
